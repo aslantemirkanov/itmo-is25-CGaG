981f5826d6ca7f3cc0407e014af87df83a678459
Aleksandr Goryunov <sasha.g27204@gmail.com>
2023-10-25 18:37:25 +0300

    floyd steinberg

--- src/main/java/ru/squad1332/cg/dither/FloydSteinberg.java
+++ src/main/java/ru/squad1332/cg/dither/FloydSteinberg.java
@@ -25,34 +25,38 @@ public class FloydSteinberg extends DitheringAlgorithm {
                     }
                 }
                 double newValue = (Math.abs(pixel.getFirst() - values[0]) < Math.abs(pixel.getFirst() - values[1])) ? values[0] : values[1];
+                pixels[pointer] = new Pixel(newValue, newValue, newValue);
 
                 double error = pixel.getFirst() - newValue;
                 if (x + 1 < w) {
                     Pixel rightPixel = pixels[pointer + 1];
-                    rightPixel.setFirst(error * 7 / 16);
-                    rightPixel.setSecond(error * 7 / 16);
-                    rightPixel.setThird(error * 7 / 16);
+                    double newPixel = Math.min(Math.max(0, rightPixel.getFirst() + error * 7 / 16), 1);
+                    rightPixel.setFirst(newPixel);
+                    rightPixel.setSecond(newPixel);
+                    rightPixel.setThird(newPixel);
                 }
 
                 if (y + 1 < h) {
                     if (x > 0) {
                         Pixel bottomLeftPixel = pixels[pointer + w - 1];
-                        bottomLeftPixel.setFirst(error * 3 / 16);
-                        bottomLeftPixel.setSecond(error * 3 / 16);
-                        bottomLeftPixel.setThird(error * 3 / 16);
+                        double newPixel = Math.min(Math.max(0, bottomLeftPixel.getFirst() + error * 3 / 16), 1);
+                        bottomLeftPixel.setFirst(newPixel);
+                        bottomLeftPixel.setSecond(newPixel);
+                        bottomLeftPixel.setThird(newPixel);
                     }
 
                     Pixel bottomPixel = pixels[pointer + w];
-                    bottomPixel.setFirst(error * 5 / 16);
-                    bottomPixel.setSecond(error * 5 / 16);
-                    bottomPixel.setThird(error * 5 / 16);
+                    double newPixel = Math.min(Math.max(0, bottomPixel.getFirst() + error * 5 / 16), 1);
+                    bottomPixel.setFirst(newPixel);
+                    bottomPixel.setSecond(newPixel);
+                    bottomPixel.setThird(newPixel);
 
                     if (x + 1 < w) {
                         Pixel bottomRightPixel = pixels[pointer + w + 1];
-
-                        bottomRightPixel.setFirst(error * 1 / 16);
-                        bottomRightPixel.setSecond(error * 1 / 16);
-                        bottomRightPixel.setThird(error * 1 / 16);
+                        newPixel = Math.min(Math.max(0, bottomRightPixel.getFirst() + error * 1 / 16), 1);
+                        bottomRightPixel.setFirst(newPixel);
+                        bottomRightPixel.setSecond(newPixel);
+                        bottomRightPixel.setThird(newPixel);
                     }
                 }
             }
