b79b5232cc7f50d27660ff079c1622e2395d9f3a
Aleksandr Goryunov <sasha.g27204@gmail.com>
2023-10-25 19:03:03 +0300

    atkinson

--- src/main/java/ru/squad1332/cg/dither/DitheringService.java
+++ src/main/java/ru/squad1332/cg/dither/DitheringService.java
@@ -7,7 +7,8 @@ import java.util.Map;
 public class DitheringService {
     private static final Map<String, DitheringAlgorithm> DITHER_BY_NAME = Map.of("Ordered", new Ordered(),
             "Random", new Random(),
-            "Floyd-Steinberg", new FloydSteinberg());
+            "Floyd-Steinberg", new FloydSteinberg(),
+            "Atkinson", new Atkinson());
     public static void applyDithering(Pixel[] picture, String mode, int bit, int w, int h) {
         DITHER_BY_NAME.get(mode).apply(picture, bit, w, h);
     }
